a7237f9958f8a7cf0686fd03d63f2b148f953b8e
thinkofher <piaskupiasku@gmail.com>
2020-11-02 00:35:13 +0100

    Retrieving values from context.
    * Created private generic function for retrieving string values from context.

--- pkg/services/requests/requests.go
+++ pkg/services/requests/requests.go
@@ -12,9 +12,8 @@ import (
 	"github.com/hakierspejs/long-season/pkg/services/config"
 )
 
-// UserID returns user id from url.
-func UserID(r *http.Request) (int, error) {
-	id, ok := r.Context().Value("user-id").(string)
+func id(key string, r *http.Request) (int, error) {
+	id, ok := r.Context().Value(key).(string)
 	if !ok {
 		return 0, errors.New("ID stored in context has inapropriate type.")
 	}
@@ -25,6 +24,15 @@ func UserID(r *http.Request) (int, error) {
 	}
 
 	return res, nil
+}
+
+// UserID returns user id from url.
+func UserID(r *http.Request) (int, error) {
+	return id("user-id", r)
+}
+
+func DeviceID(r *http.Request) (int, error) {
+	return id("device-id", r)
 }
 
 // JWTClaims retrieves jwt claims specific for long-season from
