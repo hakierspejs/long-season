489df50d6f85daa8b0cf5ba0c1531ac9b12c013f
thinkofher <piaskupiasku@gmail.com>
2020-12-16 07:13:17 +0100

    Port short-season from flag to urfave/cli/v2.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
-	"flag"
 	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func putRequest(url string, headers map[string]string, data io.Reader) (*http.Response, error) {
@@ -33,46 +34,85 @@ type body struct {
 	Addresses []string `json:"addresses"`
 }
 
-var (
-	api    string
-	apiKey string
-)
+func app() *cli.App {
+	return &cli.App{
+		Name:  "short-season",
+		Usage: "command line interface tool for managing long-season",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "api",
+				Value: "",
+				Usage: "api address",
+			},
+			&cli.StringFlag{
+				Name:  "api-key",
+				Value: "",
+				Usage: "api key updating statuses",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "macs",
+				Usage: "upload list of mac addresses to given long-season API",
+				Action: func(ctx *cli.Context) error {
+					api := ctx.String("api")
+					apiKey := ctx.String("api-key")
 
-func run() error {
-	flag.StringVar(&api, "api", "", "api address")
-	flag.StringVar(&apiKey, "api-key", "", "api key for updating statuses")
-	flag.Parse()
+					b := new(body)
 
-	b := new(body)
+					scanner := bufio.NewScanner(os.Stdin)
 
-	scanner := bufio.NewScanner(os.Stdin)
+					for scanner.Scan() {
+						b.Addresses = append(b.Addresses, scanner.Text())
+					}
+					if err := scanner.Err(); err != nil {
+						return err
+					}
 
-	for scanner.Scan() {
-		b.Addresses = append(b.Addresses, scanner.Text())
+					body, err := json.Marshal(b)
+					if err != nil {
+						return err
+					}
+
+					buff := bytes.NewBuffer(body)
+
+					headers := map[string]string{
+						"Authorization": "Status " + apiKey,
+					}
+					_, err = putRequest(api+"/api/v1/update", headers, buff)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "admin",
+				Usage: "set of administration tools for managing content of long-season database",
+				Flags: []cli.Flag{},
+				Subcommands: []*cli.Command{
+					{
+						Name: "users",
+						Subcommands: []*cli.Command{
+							{
+								Name: "delete",
+							},
+							{
+								Name: "add",
+							},
+							{
+								Name: "edit",
+							},
+						},
+					},
+				},
+			},
+		},
 	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-
-	body, err := json.Marshal(b)
-	if err != nil {
-		return err
-	}
-
-	buff := bytes.NewBuffer(body)
-
-	headers := map[string]string{
-		"Authorization": "Status " + apiKey,
-	}
-	_, err = putRequest(api+"/api/v1/update", headers, buff)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	if err := app().Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
