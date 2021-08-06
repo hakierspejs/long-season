3ae83df6da3c78d7f116e621998033590383fadd
thinkofher <mail@bdudek.xyz>
2021-08-06 14:03:38 +0200

    Add docs.

--- pkg/services/happier/happier.go
+++ pkg/services/happier/happier.go
@@ -10,11 +10,89 @@ import (
 	"github.com/thinkofher/horror"
 )
 
+// Factory contains method for declaring
+// http oriented errors.
+type Factory struct {
+	debug bool
+}
+
+// FromRequest returns factory that should be used only with
+// given http request.
+func FromRequest(r *http.Request) *Factory {
+	debug, err := requests.Debug(r)
+	if err != nil {
+		debug = false
+	}
+	return &Factory{
+		debug: debug,
+	}
+}
+
+// NotFound implements http NotFound (404) error for horror.Error
+// interface to use in long-season REST API.
+func (f *Factory) NotFound(err error, message string) horror.Error {
+	return &errorHandler{
+		message: message,
+		wrapped: err,
+		code:    http.StatusNotFound,
+		debug:   f.debug,
+	}
+}
+
+// InternalServerError implements http internal server error (500)
+// for horror.Error interface to use in long-season REST API.
+func (f *Factory) InternalServerError(err error, message string) horror.Error {
+	return &errorHandler{
+		message: message,
+		wrapped: err,
+		code:    http.StatusInternalServerError,
+		debug:   f.debug,
+	}
+}
+
+// Conflict implements http conflict (409) error for horror.Error
+// interface to use in long-season REST API.
+func (f *Factory) Conflict(err error, message string) horror.Error {
+	return &errorHandler{
+		message: message,
+		wrapped: err,
+		code:    http.StatusConflict,
+		debug:   f.debug,
+	}
+}
+
+// BadRequest implements http bad request (400) error for horror.Error
+// interface to use in long-season REST API.
+func (f *Factory) BadRequest(err error, message string) horror.Error {
+	return &errorHandler{
+		message: message,
+		wrapped: err,
+		code:    http.StatusBadRequest,
+		debug:   f.debug,
+	}
+}
+
 type errorHandler struct {
 	message string
 	wrapped error
 	code    int
 	debug   bool
+}
+
+func (e *errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	res := &errorResponse{
+		Data: &dataResponse{
+			Code:    e.code,
+			Message: e.message,
+		},
+	}
+
+	if e.debug {
+		res.Debug = &debugResponse{
+			ErrorMessage: e.wrapped.Error(),
+		}
+	}
+	gores.JSONIndent(w, e.code, res, defaultPrefix, defaultIndent)
 }
 
 type errorResponse struct {
@@ -43,71 +121,3 @@ const (
 	defaultPrefix string = ""
 	defaultIndent        = "    "
 )
-
-func (e *errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	res := &errorResponse{
-		Data: &dataResponse{
-			Code:    e.code,
-			Message: e.message,
-		},
-	}
-
-	if e.debug {
-		res.Debug = &debugResponse{
-			ErrorMessage: e.wrapped.Error(),
-		}
-	}
-	gores.JSONIndent(w, e.code, res, defaultPrefix, defaultIndent)
-}
-
-// Factory contains method for declaring
-// http oriented errors.
-type Factory struct {
-	debug bool
-}
-
-func FromRequest(r *http.Request) *Factory {
-	debug, err := requests.Debug(r)
-	if err != nil {
-		debug = false
-	}
-	return &Factory{
-		debug: debug,
-	}
-}
-
-func (f *Factory) NotFound(err error, message string) horror.Error {
-	return &errorHandler{
-		message: message,
-		wrapped: err,
-		code:    http.StatusNotFound,
-		debug:   f.debug,
-	}
-}
-
-func (f *Factory) InternalServerError(err error, message string) horror.Error {
-	return &errorHandler{
-		message: message,
-		wrapped: err,
-		code:    http.StatusInternalServerError,
-		debug:   f.debug,
-	}
-}
-
-func (f *Factory) Conflict(err error, message string) horror.Error {
-	return &errorHandler{
-		message: message,
-		wrapped: err,
-		code:    http.StatusConflict,
-		debug:   f.debug,
-	}
-}
-
-func (f *Factory) BadRequest(err error, message string) horror.Error {
-	return &errorHandler{
-		message: message,
-		wrapped: err,
-		code:    http.StatusBadRequest,
-		debug:   f.debug,
-	}
-}
