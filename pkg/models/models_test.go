00438248556d77678c007fbf3338e1b1281986c2
thinkofher <mail@bdudek.xyz>
2021-09-23 03:07:29 +0200

    Fix tests for models.

--- pkg/models/models_test.go
+++ pkg/models/models_test.go
@@ -10,19 +10,22 @@ import (
 func TestParsingTwoFactor(t *testing.T) {
 	is := is.New(t)
 	dat := []byte(`{
-		"oneTimeCodes": [{
+		"oneTimeCodes": {
+		"1": {
 			"id": "someid",
 			"name": "somename",
 			"randomKey": "randomValue"
-		},{
+		},
+		"2": {
 			"name": "sda",
 			"secret": "somesecret"
-		}]
+		}
+		}
 	}`)
 	res := new(TwoFactor)
 	is.NoErr(json.Unmarshal(dat, res))
-	is.Equal(res.OneTimeCodes[0].ID, "someid")
-	is.Equal(res.OneTimeCodes[0].Name, "somename")
-	is.Equal(res.OneTimeCodes[1].Name, "sda")
-	is.Equal(res.OneTimeCodes[1].Secret, "somesecret")
+	is.Equal(res.OneTimeCodes["1"].ID, "someid")
+	is.Equal(res.OneTimeCodes["1"].Name, "somename")
+	is.Equal(res.OneTimeCodes["2"].Name, "sda")
+	is.Equal(res.OneTimeCodes["2"].Secret, "somesecret")
 }
